b802d3934aa49af95a04ca6f6cfea31b97429fbc
Saptorshi Majumder <52661102+SamRyder27@users.noreply.github.com>
2026-01-30 20:44:27 +0530

    Day 1 JWT impl
    jwtutil package is created and jwt class are in process

--- channel-chat-service/src/main/java/com/channel_chat_service/JwtUtils/JwtUtil.java
+++ channel-chat-service/src/main/java/com/channel_chat_service/JwtUtils/JwtUtil.java
@@ -1,12 +1,21 @@
 package com.channel_chat_service.JwtUtils;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.net.http.HttpRequest;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -27,4 +36,46 @@ public class JwtUtil {
         }
         return null;
     }
+
+    //creating new token after checking a valid user
+    public String generateTokenFromUsername (UserDetails userDetails){
+        String username = userDetails.getUsername();
+        String token = Jwts.builder().subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpiration))
+                .signWith (key())
+                .compact();
+        return token;
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    //getting the username/ userdetails from a valid bearer token given at user end
+    public String getUserNameFromJwtToken (String token){
+        String username = Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+        return username;
+    }
+
+    public boolean validateJwtToken (String authToekn){
+        try{
+            System.out.println("Validating Auth Token");
+            Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(authToekn);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT Token: {} ", e.getMessage());
+        } catch (ExpiredJwtException e){
+            logger.error("JWT Token is expired: {} ", e.getMessage());
+        } catch (UnsupportedJwtException e){
+            logger.error("JWT Token is not supported: {} ", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.error ("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
 }
