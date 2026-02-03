94b300460d364f98c5eb2ccc404a2c8c9f0f72e7
Saptorshi Majumder <52661102+SamRyder27@users.noreply.github.com>
2026-02-03 21:16:36 +0530

    cmt
    cmt

--- channel-chat-service/src/main/java/com/channel_chat_service/Config/SecurityConfig.java
+++ channel-chat-service/src/main/java/com/channel_chat_service/Config/SecurityConfig.java
@@ -1,4 +1,72 @@
 package com.channel_chat_service.Config;
 
+import com.channel_chat_service.JwtUtils.JwtAuthEntryPoint;
+import com.channel_chat_service.JwtUtils.JwtAuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JwtAuthEntryPoint unauthorisedhandler;
+    //@Autowired
+    //private JwtAuthTokenFilter jwtAuthTokenFilter;
+
+    public SecurityConfig(DataSource dataSource, JwtAuthEntryPoint unauthorisedhandler) {
+        this.dataSource = dataSource;
+        this.unauthorisedhandler = unauthorisedhandler;
+       // this.jwtAuthTokenFilter = jwtAuthTokenFilter;
+    }
+
+    public  JwtAuthTokenFilter jwtAuthTokenFilter (){
+        return  new JwtAuthTokenFilter();
+    }
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+
+
+        http.authorizeHttpRequests(authRequest -> authRequest
+                .requestMatchers("/console/signup/").permitAll()
+                .requestMatchers("/api/signin/").permitAll()
+                .anyRequest().authenticated()
+        );
+        http.sessionManagement(session -> session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS)
+        );
+
+        http.exceptionHandling((exception -> exception.authenticationEntryPoint(unauthorisedhandler)));
+        http.headers( header -> header.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        http.addFilterBefore(jwtAuthTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder){
+        return builder.getAuthenticationManager();
+    }
+
 }
---
+++ channel-chat-service/src/main/java/com/channel_chat_service/JwtUtils/JwtAuthTokenFilter.java
@@ -0,0 +1,55 @@
+package com.channel_chat_service.JwtUtils;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtUtil jwtUtil;
+    @Autowired
+    UserDetailsService userDetailsService;
+
+//    public JwtAuthTokenFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+//        this.jwtUtil = jwtUtil;
+//        this.userDetailsService = userDetailsService;
+//    }
+
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 FilterChain filterChain) throws ServletException, IOException {
+        logger.debug("AuthTokenFilter called for URI: {} ");
+        try {
+            String jwtToken = jwtUtil.getJwtFromHeader(request);
+            if (jwtToken != null && jwtUtil.validateJwtToken(jwtToken)) {
+                String username = jwtUtil.getUserNameFromJwtToken(jwtToken);
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,
+                            null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Cannot authenticate the user {} ", e);
+        }
+        filterChain.doFilter(request, response);
+        super.doFilter(request, response, filterChain);
+    }
+}
